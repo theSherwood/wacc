59f8e050a7b3a2c4550a4eb6333b544758e4ff8b
Adam Sherwood <adam.the.sherwood@gmail.com>
2025-07-16 21:06:00 +0100

    adds tests

---
+++ tests/runtime/if_else_block_nested.c
@@ -0,0 +1,13 @@
+int main() {
+  if (1) {
+    if (1) {
+      return 42;
+    } else {
+      return 32;
+    }
+  } else {
+    return 10;
+  }
+}
+
+// Expected: 42
---
+++ tests/runtime/if_else_block_nested_3.c
@@ -0,0 +1,13 @@
+int main() {
+  if (0) {
+    return 10;
+  } else {
+    if (1) {
+      return 42;
+    } else {
+      return 32;
+    }
+  }
+}
+
+// Expected: 42
